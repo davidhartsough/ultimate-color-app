10e5f1c90c548eb9316247da206df6f72db98419
David Hartsough <hartsoughdavid@gmail.com>
2020-01-08 12:32:39 -0700

    Updates

---
+++ precache-manifest.622ff214d9e432045741595fd7c9d7c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd1a41fdcc1fca531a5f32e78a955554",
+    "url": "/ultimate-color-app/index.html"
+  },
+  {
+    "revision": "b485ea64590040e975e3",
+    "url": "/ultimate-color-app/static/css/main.a3127dee.chunk.css"
+  },
+  {
+    "revision": "3fdc56b2e1d837352bc6",
+    "url": "/ultimate-color-app/static/js/2.8141010b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ultimate-color-app/static/js/2.8141010b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b485ea64590040e975e3",
+    "url": "/ultimate-color-app/static/js/main.2131922b.chunk.js"
+  },
+  {
+    "revision": "70b1dd4444321a4b4013",
+    "url": "/ultimate-color-app/static/js/runtime-main.edb1681c.js"
+  }
+]);
